c50d4a8c6b6ae9ccadb463b2a18dad05a2820ca8
Christian H <christian.hjelseth@gmail.com>
2019-07-15 23:48:54 +0200

    Add summary to overview

--- HealthCheck.Frontend/src/components/Pages/OverviewPageComponent.vue
+++ HealthCheck.Frontend/src/components/Pages/OverviewPageComponent.vue
@@ -8,9 +8,22 @@
           <!-- CONTENT BEGIN -->
             
         <v-container grid-list-md>
-            <h1 class="mb-4">Events overview</h1>
+            <h1 class="mb-2">Events overview</h1>
 
             <v-layout align-content-center wrap>
+                
+                <v-flex sm12 md7 class="mb-4" v-if="showContent">
+                    <v-alert :value="true" :color="summaryColor" :icon="summaryIcon">
+                        <span v-if="summaryItems.length == 1">{{ summaryItems[0] }}</span>
+                        <ul v-if="summaryItems.length > 1">
+                            <li v-for="(summaryItem, index) in summaryItems"
+                                :key="`summary-item-${index}`">
+                                {{ summaryItem }}
+                            </li>
+                        </ul>
+                    </v-alert>
+                </v-flex>
+            
                 <v-alert
                     :value="overviewDataLoadFailed"
                     type="error">
@@ -21,11 +34,7 @@
                     v-if="overviewDataLoadInProgress"
                     indeterminate color="green"></v-progress-linear>
                     
-                <v-flex sm12 md7
-                    v-if="showContent">
-                    <v-alert :value="calendarEvents.length == 0" color="info" icon="sentiment_satisfied_alt" outline>
-                        Things are quiet, nothing seems to have happened lately.
-                    </v-alert>
+                <v-flex sm12 md7 v-if="showContent">
                     <event-calendar-component
                         :events="calendarEvents"
                         class="calendar" />
@@ -89,13 +98,66 @@ export default class OverviewPageComponent extends Vue {
     get calendarEvents(): Array<SiteEventViewModel> {
         return this.siteEvents;
     }
+
     get timelineEvents(): Array<SiteEventViewModel> {
         let thresholdDate = new Date();
         thresholdDate.setDate(thresholdDate.getDate() - 3);
         return this.siteEvents.filter(x => x.Timestamp >= thresholdDate);
     }
+
     get showContent(): boolean {
         return !this.overviewDataLoadInProgress && !this.overviewDataLoadFailed;
+    }
+
+    get summaryItems(): Array<string> {
+        let relevantEvents = this.getSummaryEvents();
+        if (relevantEvents.length == 0) {
+            return ["All Systems Operational."];
+        }
+        
+        let severity = this.getSummarySeverity();
+        let relevantMessages = relevantEvents
+            .filter(x => x.Severity == severity)
+            .map(x => x.Title);
+        return relevantMessages;
+    }
+
+    get summaryColor(): string {
+        let severity = this.getSummarySeverity();
+        if (severity == null) {
+            return "success";
+        }
+        else if (severity == SiteEventSeverity.Information) {
+            return "info";
+        }
+        else if (severity == SiteEventSeverity.Warning) {
+            return "warning";
+        }
+        else if (severity == SiteEventSeverity.Error || severity == SiteEventSeverity.Fatal) {
+            return "error";
+        }
+        else {
+            return "info";
+        }
+    }
+
+    get summaryIcon(): string {
+        let severity = this.getSummarySeverity();
+        if (severity == null) {
+            return "sentiment_satisfied_alt";
+        }
+        else if (severity == SiteEventSeverity.Information) {
+            return "info";
+        }
+        else if (severity == SiteEventSeverity.Warning) {
+            return "warning";
+        }
+        else if (severity == SiteEventSeverity.Error || severity == SiteEventSeverity.Fatal) {
+            return "error";
+        }
+        else {
+            return "sentiment_satisfied_alt";
+        }
     }
 
     ////////////////
@@ -134,6 +196,29 @@ export default class OverviewPageComponent extends Vue {
             x.Timestamp = new Date(x.Timestamp);
         });
         this.siteEvents = events;
+    }
+
+    getSummaryEvents(): Array<SiteEventViewModel> {
+        let thresholdDate = new Date();
+        thresholdDate.setHours(thresholdDate.getHours() - 1);
+        return this.siteEvents.filter(x => x.Timestamp >= thresholdDate);
+    }
+
+    getSummarySeverity(): SiteEventSeverity | null {
+        let relevantEvents = this.getSummaryEvents();
+        if (relevantEvents.length == 0) {
+            return null;
+        } else if(relevantEvents.some(x => x.Severity == SiteEventSeverity.Fatal)) {
+            return SiteEventSeverity.Fatal;
+        } else if(relevantEvents.some(x => x.Severity == SiteEventSeverity.Error)) {
+            return SiteEventSeverity.Error;
+        } else if(relevantEvents.some(x => x.Severity == SiteEventSeverity.Warning)) {
+            return SiteEventSeverity.Warning;
+        } else if(relevantEvents.some(x => x.Severity == SiteEventSeverity.Information)) {
+            return SiteEventSeverity.Information;
+        } else {
+            return null;
+        }
     }
 }
 </script>
