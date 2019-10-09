d38446e869dbccb05bf7fc0a752861c2eb9268cc
Christian H <christian.hjelseth@gmail.com>
2019-10-09 17:29:00 +0200

    Remove unused node package

--- HealthCheck.Frontend/src/index.ts
+++ HealthCheck.Frontend/src/index.ts
@@ -3,18 +3,6 @@ import Vue from "vue";
 import HealthCheckPageComponent from "./components/HealthCheckPageComponent.vue";
 import Vuetify from 'vuetify'
 
-// Highlight.js
-import VueHighlightJS from 'vue-highlight.js';
-import xml from 'highlight.js/lib/languages/xml';
-import json from 'highlight.js/lib/languages/json';
-import 'highlight.js/styles/default.css';
-Vue.use(VueHighlightJS, {
-	languages: {
-		xml,
-		json
-	}
-});
-
 // Extensions
 import './util/extensions/StringExtensions';
 
