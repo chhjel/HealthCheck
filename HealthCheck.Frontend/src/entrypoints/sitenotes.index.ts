ec6797fbc1efa7466559335639cd76017792e80f
Christian H <christian.hjelseth@gmail.com>
2019-12-08 01:09:37 +0100

    Add injected app div

--- HealthCheck.Frontend/src/entrypoints/sitenotes.index.ts
+++ HealthCheck.Frontend/src/entrypoints/sitenotes.index.ts
@@ -21,10 +21,13 @@ Vue.use(Vuetify, {
     }
 });
 
-        // <health-check-page-component :options="options" />
+let elId = 'sitenotes-app-c26c1905-3bd4-4f7f-8671-c5ce508d7b23';
+var elem = document.createElement('div');
+elem.id = elId;
+document.body.appendChild(elem);
         
 let v = new Vue({
-    el: "#app",
+    el: `#${elId}`,
     template: `
     <div>
         woop
@@ -35,5 +38,6 @@ let v = new Vue({
     },
     components: {
         // HealthCheckPageComponent
+        // <health-check-page-component :options="options" />
     }
 });
