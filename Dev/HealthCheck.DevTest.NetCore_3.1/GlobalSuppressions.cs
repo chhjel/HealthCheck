2e8032379196a7e905f77ccd124fa55c74da65bc
Christian H <christian.hjelseth@gmail.com>
2022-02-09 09:25:18 +0100

    Remove old net core 3.1 dev project

--- Dev/HealthCheck.DevTest.NetCore_3.1/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "Dev code")]
-[assembly: SuppressMessage("Critical Code Smell", "S927:parameter names should match base declaration and other partial definitions", Justification = "Dev code")]
