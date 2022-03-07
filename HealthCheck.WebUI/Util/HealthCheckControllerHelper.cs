7e278e0f9b488aa14e8c1f00a2c47e66ae33871b
Christian H <christian.hjelseth@gmail.com>
2022-03-07 21:50:21 +0100

    Add app loader

--- HealthCheck.WebUI/Util/HealthCheckControllerHelper.cs
+++ HealthCheck.WebUI/Util/HealthCheckControllerHelper.cs
@@ -650,6 +650,7 @@ namespace HealthCheck.WebUI.Util
             var serializer = new NewtonsoftJsonSerializer();
 
             var noIndexMeta = pageOptions.IncludeNoIndex ? $"<meta name={Q}robots{Q} content={Q}noindex{Q}>" : "";
+            var loaderStyling = CreateLoaderStyling();
 
             return $@"
 <!doctype html>
@@ -661,10 +662,17 @@ namespace HealthCheck.WebUI.Util
     <meta name={Q}viewport{Q} content={Q}width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui{Q}>
     {cssTagsHtml}
     {pageOptions.CustomHeadHtml}
+    {loaderStyling}
 </head>
 
 <body>
     <div id={Q}app{Q}></div>
+    <div id={Q}app-loader{Q} class={Q}floating-squares-effect{Q}>
+        <div class={Q}app-loader-text{Q}>[LOADING]</div>
+        <ul class={Q}bg-bubbles{Q}>
+            <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+        </ul>
+    </div>
 
     <script>
         window.healthCheckOptions = {serializer.Serialize(frontEndOptions) ?? "{}"};
@@ -676,6 +684,139 @@ namespace HealthCheck.WebUI.Util
 </body>
 </html>";
         }
+
+        private static string CreateLoaderStyling()
+        {
+            return @"
+<style>
+.floating-squares-effect {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background-color: #fff;
+}
+
+.floating-squares-effect .bg-bubbles {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 1;
+}
+.floating-squares-effect .bg-bubbles li {
+	position: absolute;
+	list-style: none;
+	display: block;
+	width: 40px;
+	height: 40px;
+	background-color: #ade5ff;
+	bottom: -160px;
+	-webkit-animation: square 70s infinite;
+	animation: square 70s infinite;
+	-webkit-transition-timing-function: linear;
+	transition-timing-function: linear;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(1) {
+	left: 10%;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(2) {
+	left: 20%;
+	width: 80px;
+	height: 80px;
+	-webkit-animation-delay: 2s;
+	animation-delay: 2s;
+	-webkit-animation-duration: 57s;
+	animation-duration: 57s;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(3) {
+	left: 25%;
+	-webkit-animation-delay: 4s;
+	animation-delay: 4s;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(4) {
+	left: 40%;
+	width: 60px;
+	height: 60px;
+	-webkit-animation-duration: 62s;
+	animation-duration: 62s;
+	background-color: #91c9fa;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(5) {
+	left: 70%;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(6) {
+	left: 80%;
+	width: 120px;
+	height: 120px;
+	-webkit-animation-delay: 3s;
+	animation-delay: 3s;
+	background-color: #75aede;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(7) {
+	left: 32%;
+	width: 160px;
+	height: 160px;
+	-webkit-animation-delay: 7s;
+	animation-delay: 7s;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(8) {
+	left: 55%;
+	width: 20px;
+	height: 20px;
+	-webkit-animation-delay: 15s;
+	animation-delay: 15s;
+	-webkit-animation-duration: 88s;
+	animation-duration: 88s;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(9) {
+	left: 25%;
+	width: 10px;
+	height: 10px;
+	-webkit-animation-delay: 2s;
+	animation-delay: 2s;
+	-webkit-animation-duration: 90s;
+	animation-duration: 90s;
+	background-color: #91c9fa;
+}
+.floating-squares-effect .bg-bubbles li:nth-child(10) {
+	left: 90%;
+	width: 160px;
+	height: 160px;
+	-webkit-animation-delay: 28s;
+	animation-delay: 28s;
+}
+@-webkit-keyframes square {
+	0% {
+		-webkit-transform: translateY(0);
+		transform: translateY(0);
+	}
+	100% {
+		-webkit-transform: translateY(-1700px) rotate(600deg);
+		transform: translateY(-1700px) rotate(600deg);
+	}
+}
+@keyframes square {
+	0% {
+		-webkit-transform: translateY(0);
+		transform: translateY(0);
+	}
+	100% {
+		-webkit-transform: translateY(-1700px) rotate(600deg);
+		transform: translateY(-1700px) rotate(600deg);
+	}
+}
+.app-loader-text {
+    font-size: 34px;
+    font-family: sans-serif;
+    color: #516d87;
+    text-align: center;
+    margin-top: 12%;
+}
+</style
+";
+        }
+
         private static void ValidateConfig(HCFrontEndOptions frontEndOptions, HCPageOptions pageOptions)
         {
             frontEndOptions.Validate();
