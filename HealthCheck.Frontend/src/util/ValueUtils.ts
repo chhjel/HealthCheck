a03fd8bdafbd85687e4c552819add59d4d73aec0
Christian H <christian.hjelseth@gmail.com>
2022-03-14 20:42:39 +0100

    Add value util

---
+++ HealthCheck.Frontend/src/util/ValueUtils.ts
@@ -0,0 +1,5 @@
+export default class ValueUtils {
+	public static IsToggleTrue(val: string | boolean): boolean {
+		return (typeof val === "boolean" && val) || (typeof val === "string" && val.toLowerCase() == "true");
+	}
+}
