0e772b1fd8d1fdc53da1768c81c1e85de22b6e86
Christian H <christian.hjelseth@gmail.com>
2022-03-15 08:43:20 +0100

    Register new components globally

--- HealthCheck.Frontend/src/entry/index.ts
+++ HealthCheck.Frontend/src/entry/index.ts
@@ -1,5 +1,5 @@
 import "babel-polyfill";
-import { createApp } from 'vue'
+import { App, createApp } from 'vue'
 // import Vue from "vue";
 // import VueRouter from "vue-router";
 import HealthCheckPageComponent from "@components/HealthCheckPageComponent.vue";
@@ -43,14 +43,71 @@ if (appElement !== null)
 		.use(store)
 		.use(router)
 		.use(shadow);
-	const rootComponent = app.mount(appElement);
+	registerGlobalComponents(app);
+	app.mount(appElement);
 }
 else if (document.getElementById("app-download") !== null)
 {
 	const appElement = document.getElementById("app-download");
 
 	let props = { moduleConfig: moduleConfig };
-	createApp(DownloadPageComponent, props)
-		.use(shadow)
-		.mount(appElement);
+	const app = createApp(DownloadPageComponent, props)
+		.use(shadow);
+	registerGlobalComponents(app);
+	app.mount(appElement);
+}
+
+import ToolbarComponent from '@components/Common/Basic/ToolbarComponent.vue';
+import BtnComponent from '@components/Common/Basic/BtnComponent.vue';
+import IconComponent from '@components/Common/Basic/IconComponent.vue';
+import ContentComponent from '@components/Common/Basic/ContentComponent.vue';
+import AlertComponent from '@components/Common/Basic/AlertComponent.vue';
+import ProgressLinearComponent from '@components/Common/Basic/ProgressLinearComponent.vue';
+import DialogComponent from '@components/Common/Basic/DialogComponent.vue';
+import CardComponent from '@components/Common/Basic/CardComponent.vue';
+import ListComponent from '@components/Common/Basic/ListComponent.vue';
+import BadgeComponent from '@components/Common/Basic/BadgeComponent.vue';
+import TooltipComponent from '@components/Common/Basic/TooltipComponent.vue';
+import TextareaComponent from '@components/Common/Basic/TextareaComponent.vue';
+import ChipComponent from '@components/Common/Basic/ChipComponent.vue';
+import AutocompleteComponent from '@components/Common/Basic/AutocompleteComponent.vue';
+import NavigationDrawerComponent from '@components/Common/Basic/NavigationDrawerComponent.vue';
+import MenuComponent from '@components/Common/Basic/MenuComponent.vue';
+import ComboboxComponent from '@components/Common/Basic/ComboboxComponent.vue';
+import ExpansionPanelComponent from '@components/Common/Basic/ExpansionPanelComponent.vue';
+import PaginationComponent from '@components/Common/Basic/PaginationComponent.vue';
+import DatePickerComponent from '@components/Common/Basic/DatePickerComponent.vue';
+import CalendarComponent from '@components/Common/Basic/CalendarComponent.vue';
+import TimelineComponent from '@components/Common/Basic/TimelineComponent.vue';
+import ProgressCircularComponent from '@components/Common/Basic/ProgressCircularComponent.vue';
+import SnackbarComponent from '@components/Common/Basic/SnackbarComponent.vue';
+import CarouselComponent from '@components/Common/Basic/CarouselComponent.vue';
+import StepperComponent from '@components/Common/Basic/StepperComponent.vue';
+function registerGlobalComponents(app: App<Element>): void {
+	app.component("ToolbarComponent", ToolbarComponent);
+	app.component("BtnComponent", BtnComponent);
+	app.component("IconComponent", IconComponent);
+	app.component("ContentComponent", ContentComponent);
+	app.component("AlertComponent", AlertComponent);
+	app.component("ProgressLinearComponent", ProgressLinearComponent);
+	app.component("DialogComponent", DialogComponent);
+	app.component("CardComponent", CardComponent);
+	app.component("ListComponent", ListComponent);
+	app.component("BadgeComponent", BadgeComponent);
+	app.component("TooltipComponent", TooltipComponent);
+	app.component("TextareaComponent", TextareaComponent);
+	app.component("ChipComponent", ChipComponent);
+	app.component("AutocompleteComponent", AutocompleteComponent);
+	app.component("NavigationDrawerComponent", NavigationDrawerComponent);
+	app.component("MenuComponent", MenuComponent);
+	app.component("ComboboxComponent", ComboboxComponent);
+	app.component("ExpansionPanelComponent", ExpansionPanelComponent);
+	app.component("PaginationComponent", PaginationComponent);
+	app.component("DatePickerComponent", DatePickerComponent);
+	app.component("CalendarComponent", CalendarComponent);
+	app.component("TimelineComponent", TimelineComponent);
+	app.component("ProgressCircularComponent", ProgressCircularComponent);
+	app.component("SnackbarComponent", SnackbarComponent);
+	app.component("CarouselComponent", CarouselComponent);
+	app.component("StepperComponent", StepperComponent);
 }
