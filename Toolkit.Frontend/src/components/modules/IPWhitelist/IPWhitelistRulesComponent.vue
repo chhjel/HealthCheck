a4c24338f3505015700b64d2d951fb35bfb54ca5
Christian W <christian@lowzow.wang>
2023-05-05 20:35:37 +0200

    Style rule list

--- Toolkit.Frontend/src/components/modules/IPWhitelist/IPWhitelistRulesComponent.vue
+++ Toolkit.Frontend/src/components/modules/IPWhitelist/IPWhitelistRulesComponent.vue
@@ -3,21 +3,21 @@
     <div class="ip-whitelist-rules">
         <div v-if="!rules || rules.length == 0">- No whitelist rules created yet -</div>
         <div v-else class="ip-whitelist-rules__list">
-            <div v-for="rule in rules" :key="`rule-${rule.Id}-${id}`"
+            <div v-for="rule in sortedRules" :key="`rule-${rule.Id}-${id}`"
                 @click="onRuleClicked(rule)"
-                class="ip-whitelist-rules__item"
+                class="ip-whitelist-rules__item hoverable-lift-light"
                 :class="getRuleClasses(rule)">
-                <div>{{ rule.Name }}</div>
-                <div>{{ rule.Note }}</div>
-                <div>Enabled: {{ rule.Enabled }}</div>
-                <div>EnabledUntil: {{ rule.EnabledUntil }}</div>
+                <div class="ip-whitelist-rules__item__left">
+                    <icon-component>{{ getRuleIcon(rule) }}</icon-component>
+                </div>
+                <div class="ip-whitelist-rules__item__right">
+                    <div class="ip-whitelist-rules__item__name">{{ rule.Name }}</div>
+                    <div class="ip-whitelist-rules__item__meta" v-if="getRuleMetaText(rule) ">{{ getRuleMetaText(rule) }}</div>
+                    <div class="ip-whitelist-rules__item__note" v-if="rule.Note"><p>{{ rule.Note }}</p></div>
+                </div>
                 <hr/>
             </div>
         </div>
-        <hr/>
-        <div>todo: show note w/ ellipsis</div>
-        <div>todo: show expired/when it expires</div>
-        <div>todo: show as disabled if disabled or expired</div>
     </div>
 </template>
 
@@ -33,10 +33,13 @@ import IdUtils from "@util/IdUtils";
 import { TKIPWhitelistRule } from "@generated/Models/Module/IPWhitelist/TKIPWhitelistRule";
 import ModuleConfig from "@models/Common/ModuleConfig";
 import ValueUtils from "@util/ValueUtils";
+import IconComponent from "@components/Common/Basic/IconComponent.vue";
+import LinqUtils from "@util/LinqUtils";
 
 @Options({
     components: {
-        FilterableListComponent
+        FilterableListComponent,
+        IconComponent
     }
 })
 export default class IPWhitelistRuleComponent extends Vue {
@@ -75,6 +78,18 @@ export default class IPWhitelistRuleComponent extends Vue {
         return classes;
     }
 
+    getRuleIcon(rule: TKIPWhitelistRule): string {
+        if (!rule.Enabled) return 'do_not_disturb_on';
+        else if (this.ruleIsExpired(rule)) return 'timer_off';
+        else return '';
+    }
+
+    getRuleMetaText(rule: TKIPWhitelistRule): string {
+        if (!rule.Enabled) return 'Disabled';
+        else if (this.ruleIsExpired(rule)) return 'Expired';
+        else return '';
+    }
+
     ruleIsExpired(rule: TKIPWhitelistRule): boolean {
         if (rule.EnabledUntil == null) return false;
         
@@ -93,6 +108,16 @@ export default class IPWhitelistRuleComponent extends Vue {
         return this.dataLoadStatus.inProgress || ValueUtils.IsToggleTrue(this.loading);
     }
 
+    get sortedRules(): Array<TKIPWhitelistRule> {
+        let sorted = this.rules.map(x => x);
+        sorted.sort((a, b) =>  LinqUtils.SortByThenBy(a, b,
+            x => (!x.Enabled || this.ruleIsExpired(x)) ? 0 : 1,
+            x => x.Name,
+            false, true
+            ));
+        return sorted;
+    }
+
     ///////////////////////
     //  EVENT HANDLERS  //
     /////////////////////
@@ -106,6 +131,43 @@ export default class IPWhitelistRuleComponent extends Vue {
     }
     &__item {
         cursor: pointer;
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+        margin-top: 10px;
+        border: 2px solid var(--color--accent-darken2);
+        padding: 5px;
+        min-height: 38px;
+
+        &__left {
+            width: 40px;
+            padding-right: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        &__right {
+            flex: 1;
+            max-width: calc(100% - 50px);
+        }
+        &__name {
+            font-weight: 600;
+        }
+        &__meta {
+            font-weight: 600;
+            font-size: 12px;
+            color: var(--color--error-darken1);
+        }
+        &__note {
+            padding: 4px 0;
+            p {
+                margin: 0;
+                font-size: 13px;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+        }
     }
 }
 </style>
